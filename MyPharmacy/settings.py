51e30cdaf41748cc7f8112589171d2b602e6390e
Danial Afjeh <227421460+danialafjeh@users.noreply.github.com>
2026-05-29 18:36:38 +0330

    Update settings.py

--- MyPharmacy/settings.py
+++ MyPharmacy/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xttwu2382x*bqz+veuwbtcotjqyt*n564%-2=5@0u2o)6k1!cm'
+SECRET_KEY = 'django-insecure-private'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
